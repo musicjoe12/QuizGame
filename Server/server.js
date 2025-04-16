d5412da5833ab67f3a3428808a47248b11cd22f2
musicjoe12 <116874047+musicjoe12@users.noreply.github.com>
2025-04-16 16:44:40 +0100

    server change and new game

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true
 }));
 app.use(bodyParser.json());
